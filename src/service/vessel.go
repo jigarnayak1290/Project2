c77605d012100344e5ca9bda3d3f58defa7cfc3e
jigarnayak1290 <jigarnayak1290@gmail.com>
2023-07-18 05:39:31 +0900

    lit correction

--- src/service/vessel.go
+++ src/service/vessel.go
@@ -1,7 +1,27 @@
 package service
 
-import "github.com/jigarnayak1290/Project2/src/vessel"
+import (
+	"fmt"
+
+	"github.com/jigarnayak1290/Project2/src/vessel"
+)
 
 type VesselService struct {
-	vesselRe VesselRepo
+	vesselRe vessel.VesselRepo
+}
+
+func NewVesselService(repo vessel.VesselRepo) *VesselService {
+	return &VesselService{vesselRe: repo}
+}
+
+func (vs *VesselService) ListVessel() {
+	vessel, err := vs.vesselRe.GetAllVessel()
+
+	if err != nil {
+		fmt.Println("Failed to retrive vessle data")
+		return
+	}
+	for _, vessel := range vessel {
+		fmt.Printf("Vessel : %d, ", vessel.ID)
+	}
 }
