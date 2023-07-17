180bdc27644c8e2125fe91330b3e7d04178b77fd
jigarnayak1290 <jigarnayak1290@gmail.com>
2023-07-18 05:44:26 +0900

    local vessel get from Interface

--- src/main.go
+++ src/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/jigarnayak1290/Project2/src/service"
+	"github.com/jigarnayak1290/Project2/src/vessel"
+)
 
 func main() {
 	fmt.Println("Hello from main")
+
+	VesselRepo := vessel.DBVesselRepo{}
+
+	service := service.NewVesselService(VesselRepo)
+	service.ListVessel()
+
 }
